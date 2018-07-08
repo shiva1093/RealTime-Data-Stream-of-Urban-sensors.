c7e0841e16ba7ab6e91479a0648bcca9830b9d85
siva1093 <Shiva1993>
2018-07-08 02:11:24 +0200

    Nested CheckBox for direction field added.

--- Frontend/dashboard-react/src/components/transpotation/transportComponents/NestedCheckbox.jsx
+++ Frontend/dashboard-react/src/components/transpotation/transportComponents/NestedCheckbox.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import treeData from '../data.json'
 import { TreeSelect } from 'antd';
 import 'antd/dist/antd.css';
-
+var inArray = require('in-array');
 const SHOW_PARENT = TreeSelect.SHOW_PARENT;
 
 class NestedCheckbox extends React.Component {
@@ -15,14 +15,13 @@ class NestedCheckbox extends React.Component {
         var busName = [];
         var busNamesArray = [];
         var uniqueNames = [];
-        var busNamesAll =[];
         if(value == 'All'){
+            var valueToPush = []; // or "var valueToPush = new Object();" which is the same
             console.log('onChange ', value);   /* If the user selects ALL from the dropdown*/
-            busNamesArray= [
-                {
-                    'busName': value
-                }
-            ];
+            valueToPush["busName"] = value[0];
+            valueToPush["busDirection"] = null;
+            busName.push(valueToPush);
+            busNamesArray = busName
         }else if(extra.allCheckedNodes.length >= 1){
             extra.allCheckedNodes.forEach((nodes) => {
 
@@ -30,21 +29,48 @@ class NestedCheckbox extends React.Component {
                 {
                     var parentBusName = nodes.node.props.parent;
                     var busDirection = nodes.node.props.value;
+                    var valueToPush = []; // or "var valueToPush = new Object();" which is the same
+                    valueToPush["busName"] = parentBusName;
+                    valueToPush["busDirection"] = busDirection;
                     console.log(parentBusName);
                     console.log(busDirection);
-                    busName.push(parentBusName);
+                    busName.push(valueToPush);
+                    console.log(busName);
+                  //  alert('NodesHasnoChilderen')
                 }else {
                     //console.log(nodes) // prints correct value
                     var parentBusName = nodes.node.props.parent;
+                    var valueToPush = []; // or "var valueToPush = new Object();" which is the same
                     if(parentBusName){
                         console.log("BusName"+parentBusName)
-                        busName.push(parentBusName);
+                        valueToPush["busName"] = parentBusName;
+                        valueToPush["busDirection"] = null;
+                        busName.push(valueToPush);
+                      //  alert('else')
                     }else{
                         //console.log( nodes)
+                      //  alert('elseif')
                         nodes.children.forEach((parentHasChild) => {
                             var parentHasChild = parentHasChild.node.props.parent;
+                            var valueToPush = []; // or "var valueToPush = new Object();" which is the same
+                            var flag = 1;
                             console.log('parent has child'+ parentHasChild)
-                            busName.push(parentHasChild);
+                          //  valueToPush["busName"] = parentHasChild;
+                            for(var i =0;i<busName.length;i++)
+                            {
+                                console.log(busName)
+                                if(busName[i]['busName'] === parentHasChild)
+                                {
+                                    flag = 0;
+
+                                }
+                            }
+                            if(flag == 1){
+                                valueToPush["busName"] = parentHasChild;
+                                valueToPush["busDirection"] = null;
+                                busName.push(valueToPush);
+                            }
+                           // console.log(busName)
                         })
                     }
 
@@ -53,9 +79,6 @@ class NestedCheckbox extends React.Component {
                 busName.join();
                 uniqueNames =  busName.filter((val,id,array) => array.indexOf(val) == id); // To remove the duplicate names
                 busNamesArray= uniqueNames
-
-
-
             });
 
         }

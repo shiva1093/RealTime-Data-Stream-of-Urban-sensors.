682e428fbf0b84676301e0858d31b2e7fd07d7be
siva1093 <Shiva1993>
2018-07-14 15:00:05 +0200

    Merge branch 'master' of https://gitlab.tubit.tu-berlin.de/iosl-group3/IoT into Dev-Branch
    # Conflicts:
    #	Frontend/dashboard-react/src/views/Dashboard/Dashboard.jsx

--- Frontend/dashboard-react/src/index.js
+++ Frontend/dashboard-react/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 
-import "./assets/css/material-dashboard-react.css";
+import "assets/css/material-dashboard-react.css?v=1.2.0";
 
-import indexRoutes from "./routes/index.jsx";
+import indexRoutes from "routes/index.jsx";
 
 const hist = createBrowserHistory();
 

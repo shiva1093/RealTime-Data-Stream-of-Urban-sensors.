a7a4ca8154cc8fbae4a795394973de71618a9733
ZhangPei25 <peizhang25@gmail.com>
2018-07-25 14:31:28 +0200

    remove no use files

--- microservice-sensor-database/docker/development/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#echo "Start Mongo DB"
-#mongod --fork --logpath /data/log/mongodb.log
-#echo "Execute Mongo DB Script"
-#mongo /createUsers.js
-#echo "Stop Mongo DB"
-#mongod --shutdown
-echo "Start Mongo DB (with authentication enabled)"
-/usr/bin/mongod --config /etc/mongod.conf

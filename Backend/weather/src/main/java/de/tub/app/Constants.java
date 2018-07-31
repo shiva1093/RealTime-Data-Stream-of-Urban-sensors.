9d37792b1a8def2b59de06491853083b02326c60
Naveed Kamran <naveedkamran4@gmail.com>
2018-07-31 12:02:06 +0200

    Finalization

--- Backend/weather/src/main/java/de/tub/app/Constants.java
+++ Backend/weather/src/main/java/de/tub/app/Constants.java
@@ -4,13 +4,9 @@ public class Constants {
 
     public static final String TOPIC_EXCHANGE_NAME = "spring-boot-exchange";
 
-//    public static final String QUEUE_NAME = "spring-boot";
     public static final String EXCHANGE_NAME = "amq.topic";
     public static final String QUEUE_GENERIC_NAME = "appGenericQueue";
-//    public static final String QUEUE_SPECIFIC_NAME = "appSpecificQueue";
     public static final String ROUTING_KEY = "amq.topic";
-    public static final String QUEUE_NAME_WEATHER = "contextfencing.sensor.weather";
-    public static final String QUEUE_NAME_DAY_INFO = "contextfencing.sensor.daylight";
 
     public static final String CATEGORY_WEATHER = "weather";
     public static final String CATEGORY_SUN_INFO = "day_info";

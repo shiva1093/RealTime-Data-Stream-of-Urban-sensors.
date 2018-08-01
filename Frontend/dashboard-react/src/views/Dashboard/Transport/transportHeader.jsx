7015ccf0e46cdb2e039bc4c072e04123b1b3fd4b
Dimiter Orlinov Shopov <shopov@campus.tu-berlin.de>
2018-08-01 09:26:53 +0200

    line => transportLine

--- Frontend/dashboard-react/src/views/Dashboard/Transport/transportHeader.jsx
+++ Frontend/dashboard-react/src/views/Dashboard/Transport/transportHeader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 export const busHeader = [
     { id: 'transportType', numeric: false, disablePadding: true, label: 'Transport Type' },
-    { id: 'line', numeric: true, disablePadding: false, label: 'Bus Name' },
+    { id: 'transportLine', numeric: true, disablePadding: false, label: 'Bus Name' },
     { id: 'direction', numeric: true, disablePadding: false, label: 'Direction' },
     { id: 'transportAmountLowerBound', numeric: true, disablePadding: false, label: 'LowerBound' },
     { id: 'transportAmountUpperBound', numeric: true, disablePadding: false, label: 'UpperBound' },
